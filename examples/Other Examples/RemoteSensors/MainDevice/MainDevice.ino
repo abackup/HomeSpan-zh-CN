07b82c234673fd1a617f2a5d9de81fff0f351167
abackup <40161397+abackup@users.noreply.github.com>
2024-07-12 15:27:23 +0800

    Update MainDevice.ino

--- examples/Other Examples/RemoteSensors/MainDevice/MainDevice.ino
+++ examples/Other Examples/RemoteSensors/MainDevice/MainDevice.ino
@@ -1,43 +1,33 @@
 /*********************************************************************************
- *  MIT License
+ *  MIT 许可证
  *  
- *  Copyright (c) 2020-2022 Gregg E. Berman
+ *  Copyright (c) 2020-2024 Gregg E. Berman
  *  
  *  https://github.com/HomeSpan/HomeSpan
  *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ *  特此授予获得此软件和相关文档文件（“软件”）副本的任何人免费许可，以无限制方式处理软件，
+ *  包括但不限于使用、复制、修改、合并、发布、分发、再许可和/或销售软件副本的权利，并允许
+ *  向其提供软件的人员这样做，但须遵守以下条件：
  *  
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ *  上述版权声明和本许可声明均应包含在软件的所有副本或重要部分中。
  *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ *  软件按“原样”提供，不作任何明示或暗示的保证，包括但不限于适销性、特定用途的适用性和不
+ *  侵权性的保证。在任何情况下，作者或版权持有者均不对因软件或使用或其他处理软件而引起的
+ *  或与之相关的任何索赔、损害或其他责任承担责任，无论是合同行为、侵权行为还是其他行为。
  *  
  ********************************************************************************/
  
 ////////////////////////////////////////////////////////////
 //                                                        //
-//    HomeSpan: A HomeKit implementation for the ESP32    //
+//             HomeSpan：ESP32 的 HomeKit 实现            //
 //    ------------------------------------------------    //
 //                                                        //
-//     Demonstrates how to use SpanPoint() to implement   //
-//     two remote temperature sensors on separate ESP32   //
-//     devices.                                           //
+//     演示如何使用 SpanPoint() 在不同的                   //
+//     ESP32 设备上实现两个远程温度传感器。                 //
 //                                                        //
-//     This sketch is for the MAIN DEVICE that contains   //
-//     all the usual HomeSpan logic, plus two instances   //
-//     of SpanPoint to read temperatures from two other   //
-//     remote devices.                                    //
+//     该草图用于包含所有常见的 HomeSpan 逻辑的主设备，     //
+//     以及两个 SpanPoint 实例，用于从另外两个远程         //
+//     设备读取温度。                                      //
 //                                                        //
 ////////////////////////////////////////////////////////////
 
@@ -55,29 +45,29 @@ struct RemoteTempSensor : Service::TemperatureSensor {
 
     this->name=name;
     
-    temp=new Characteristic::CurrentTemperature(-10.0);      // set initial temperature
-    temp->setRange(-50,100);                                 // expand temperature range to allow negative values
+    temp=new Characteristic::CurrentTemperature(-10.0);      // 设置初始温度
+    temp->setRange(-50,100);                                 // 扩大温度范围以允许负值
 
-    fault=new Characteristic::StatusFault(1);                // set initial state = fault
+    fault=new Characteristic::StatusFault(1);                // 设置初始 state = fault
 
-    remoteTemp=new SpanPoint(macAddress,0,sizeof(float));    // create a SpanPoint with send size=0 and receive size=sizeof(float)
+    remoteTemp=new SpanPoint(macAddress,0,sizeof(float));    // 创建一个 SpanPoint，其发送大小 size=0 ，接收大小 size=sizeof(float)
 
-  } // end constructor
+  } // 结束构造函数
 
   void loop(){
        
-    if(remoteTemp->get(&temperature)){      // if there is data from the remote sensor
-      temp->setVal(temperature);            // update temperature
-      fault->setVal(0);                     // clear fault
+    if(remoteTemp->get(&temperature)){      // 如果有来自遥感器的数据
+      temp->setVal(temperature);            // 更新温度
+      fault->setVal(0);                     // 清除故障
        
       LOG1("Sensor %s update: Temperature=%0.2f\n",name,temperature*9/5+32);
       
-    } else if(remoteTemp->time()>60000 && !fault->getVal()){    // else if it has been a while since last update (60 seconds), and there is no current fault
-      fault->setVal(1);                                         // set fault state
+    } else if(remoteTemp->time()>60000 && !fault->getVal()){    // 否则，如果自上次更新以来已经有一段时间了（60 秒），并且当前没有故障
+      fault->setVal(1);                                         // 设置故障状态
       LOG1("Sensor %s update: FAULT\n",name);
     }
     
-  } // loop
+  } // 循环
   
 };
 
@@ -99,16 +89,16 @@ void setup() {
     new Service::AccessoryInformation();
       new Characteristic::Identify();
       new Characteristic::Name("Indoor Temp");
-    new RemoteTempSensor("Device 1","AC:67:B2:77:42:20");        // pass MAC Address of Remote Device
+    new RemoteTempSensor("Device 1","AC:67:B2:77:42:20");        // 传递远程设备的 MAC 地址
 
   new SpanAccessory();
     new Service::AccessoryInformation();
       new Characteristic::Identify(); 
       new Characteristic::Name("Outdoor Temp");
-    new RemoteTempSensor("Device 2","84:CC:A8:11:B4:84");        // pass MAC Address of Remote Device
+    new RemoteTempSensor("Device 2","84:CC:A8:11:B4:84");        // 传递远程设备的 MAC 地址
 
   
-} // end of setup()
+} // 设置结束()
 
 //////////////////////////////////////
 
@@ -116,6 +106,6 @@ void loop(){
   
   homeSpan.poll();
   
-} // end of loop()
+} // 循环结束()
 
 //////////////////////////////////////
