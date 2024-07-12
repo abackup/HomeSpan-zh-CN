6a5ea75fef5af3bac7f66501246796f2f9e0d7a4
abackup <40161397+abackup@users.noreply.github.com>
2024-07-12 14:52:32 +0800

    Update DEV_LED.h

--- examples/08-Bridges/DEV_LED.h
+++ examples/08-Bridges/DEV_LED.h
@@ -1,56 +1,55 @@
-
 ////////////////////////////////////
-//   DEVICE-SPECIFIC LED SERVICES //
+//       设备专用 LED 服务        //
 ////////////////////////////////////
 
-struct DEV_LED : Service::LightBulb {               // ON/OFF LED
+struct DEV_LED : Service::LightBulb {               // 开/关 LED
 
-  int ledPin;                                       // pin number defined for this LED
-  SpanCharacteristic *power;                        // reference to the On Characteristic
+  int ledPin;                                       // 为此 LED 定义的引脚编号
+  SpanCharacteristic *power;                        // 引用 On 特性
   
-  DEV_LED(int ledPin) : Service::LightBulb(){       // constructor() method
+  DEV_LED(int ledPin) : Service::LightBulb(){       // 构造函数（）方法
 
     power=new Characteristic::On();                 
     this->ledPin=ledPin;                            
     pinMode(ledPin,OUTPUT);                         
     
-  } // end constructor
+  } // 结束构造函数
 
-  boolean update(){                              // update() method
+  boolean update(){                              // update() 方法
 
     digitalWrite(ledPin,power->getNewVal());      
    
-    return(true);                               // return true
+    return(true);                               // 返回 true
   
-  } // update
+  } //  更新
 };
       
 //////////////////////////////////
 
-struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
+struct DEV_DimmableLED : Service::LightBulb {       // 可调光 LED
 
-  LedPin *ledPin;                                   // reference to Led Pin
-  SpanCharacteristic *power;                        // reference to the On Characteristic
-  SpanCharacteristic *level;                        // reference to the Brightness Characteristic
+  LedPin *ledPin;                                   // 引用 Led Pin
+  SpanCharacteristic *power;                        // 引用 On 特性
+  SpanCharacteristic *level;                        // 参考亮度特征
   
-  DEV_DimmableLED(int pin) : Service::LightBulb(){       // constructor() method
+  DEV_DimmableLED(int pin) : Service::LightBulb(){       // 构造函数（）方法
 
     power=new Characteristic::On();     
                 
-    level=new Characteristic::Brightness(50);       // Brightness Characteristic with an initial value of 50%
-    level->setRange(5,100,1);                       // sets the range of the Brightness to be from a min of 5%, to a max of 100%, in steps of 1%
+    level=new Characteristic::Brightness(50);       // 亮度特性初始值为 50%
+    level->setRange(5,100,1);                       // 将亮度范围设置为最小 5% 到最大 100%，步长为 1%
 
-    this->ledPin=new LedPin(pin);                   // configures a PWM LED for output to the specified pin
+    this->ledPin=new LedPin(pin);                   // 配置 PWM LED 以输出至指定引脚
     
-  } // end constructor
+  } // 结束构造函数
 
-  boolean update(){                              // update() method
+  boolean update(){                              // update() 方法
     
     ledPin->set(power->getNewVal()*level->getNewVal());    
    
-    return(true);                               // return true
+    return(true);                               // 返回 true
   
-  } // update
+  } //  更新
 };
       
 //////////////////////////////////
