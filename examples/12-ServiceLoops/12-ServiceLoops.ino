287a33b6ca723b9c4b2b8faa797a4a732e4c9de8
abackup <40161397+abackup@users.noreply.github.com>
2024-07-12 14:56:11 +0800

    Update 12-ServiceLoops.ino

--- examples/12-ServiceLoops/12-ServiceLoops.ino
+++ examples/12-ServiceLoops/12-ServiceLoops.ino
@@ -1,38 +1,30 @@
 /*********************************************************************************
- *  MIT License
+ *  MIT 许可证
  *  
- *  Copyright (c) 2020-2024 Gregg E. Berman
+ *  Copyright (c) 2020-2022 Gregg E. Berman
  *  
  *  https://github.com/HomeSpan/HomeSpan
  *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ *  特此授予获得此软件和相关文档文件（“软件”）副本的任何人免费许可，以无限制方式处理软件，
+ *  包括但不限于使用、复制、修改、合并、发布、分发、再许可和/或销售软件副本的权利，并允许
+ *  向其提供软件的人员这样做，但须遵守以下条件：
  *  
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ *  上述版权声明和本许可声明均应包含在软件的所有副本或重要部分中。
  *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ *  软件按“原样”提供，不作任何明示或暗示的保证，包括但不限于适销性、特定用途的适用性和不
+ *  侵权性的保证。在任何情况下，作者或版权持有者均不对因软件或使用或其他处理软件而引起的
+ *  或与之相关的任何索赔、损害或其他责任承担责任，无论是合同行为、侵权行为还是其他行为。
  *  
  ********************************************************************************/
  
 ////////////////////////////////////////////////////////////
 //                                                        //
-//    HomeSpan: A HomeKit implementation for the ESP32    //
+//              HomeSpan：ESP32 的 HomeKit 实现           //
 //    ------------------------------------------------    //
 //                                                        //
-// Example 12: Service Loops (and Event Notifications)    //
-//             * implementing a Temperature Sensor        //
-//             * implementing an Air Quality Sensor       //
+//            示例 12：服务循环（和事件通知）               //
+//                    * 实现温度传感器                     //
+//                    * 实现空气质量传感器                 //
 //                                                        //
 ////////////////////////////////////////////////////////////
 
@@ -41,50 +33,41 @@
 
 void setup() {
 
-  // So far we've seen that HomeSpan allows you to create derived Services with their own constructors and update() methods.  For many applications, this
-  // will be all that is needed.  However, for many other types of applications you may need to take action or perform some background operations without
-  // any prompting or requests from HomeKit.
+  // 到目前为止，我们已经看到 HomeSpan 允许您使用自己的构造函数和 update() 方法创建派生服务。对于许多应用程序来说，这将是所需的全部。
+  // 但是，对于许多其他类型的应用程序，您可能需要采取行动或执行一些后台操作，而无需 HomeKit 的任何提示或请求。
 
-  // To perform background operations and actions, every Service implements a loop() method.  The default loop() method is to do nothing, which has been
-  // fine for all our prior examples.  But if you need to perform some continuous background action, all you need to do is implement a loop() method for
-  // your derived Service.  At the end of each HomeSpan polling cycle, the loop() method is called for each Service that implements its own code.
-  // In this fashion, the loop() method is similar to the main loop() method in the Arduino IDE itself - except it can be customized for each Service.
+  // 要执行后台操作和动作，每个服务都实现一个 loop() 方法。默认的 loop() 方法不执行任何操作，这对于我们之前的所有示例来说都是没问题的。
+  // 但如果您需要执行一些连续的后台操作，您需要做的就是为派生服务实现一个 loop() 方法。在每个 HomeSpan 轮询周期结束时，将为每个实现其
+  // 自己代码的服务调用 loop() 方法。以这种方式，loop() 方法类似于 Arduino IDE 本身中的主 loop() 方法 - 但它可以针对每个服务进行自定义。
 
-  // In this Example 12 we explore the use of loop() methods to implement two new accessories - a Temperature Sensor and an Air Quality Sensor.  Of course
-  // we won't actually have these physical devices attached to the ESP32 for the purpose of this example, but we will simulate "reading" their properties.
-  // This is one of the main purposes of implementing a loop() method.  It allows you to read a sensor or perform some sort of repetitive, Service-specific
-  // action.
+  // 在此示例 12 中，我们探索了使用 loop() 方法来实现两个新配件 - 温度传感器和空气质量传感器。当然，为了本示例的目的，我们实际上不会将
+  // 这些物理设备连接到 ESP32，但我们将模拟“读取”它们的属性。这是实现 loop() 方法的主要目的之一。它允许您读取传感器或执行某种重复的、特定于服务的操作。
 
-  // Once you read (or simulate reading) a sensor's values in a loop() method, you need to somehow communicate this back to HomeKit so the new values can be 
-  // reflected in the HomeKit Controller.  This may be strictly for information purposes (such as a temperature sensor) or could be used by HomeKit itself
-  // to trigger other devices (as might occur if implementing a Door Sensor).
+  // 一旦您在 loop() 方法中读取（或模拟读取）传感器的值，您需要以某种方式将其传回 HomeKit，以便新值可以反映在 HomeKit 控制器中。
+  // 这可能严格用于信息目的（例如温度传感器），也可能由 HomeKit 本身用来触发其他设备（如果实现门传感器，可能会发生这种情况）。
 
-  // Fortunately, HomeSpan makes communicating the values of Characteristics back to HomeKit easy.  In prior examples we saw how getVal() and getNewVal()
-  // are used to read current and updated Characteristic values requested by HomeKit.  To perform the reverse, we simply use a method called setVal().
-  // Setting the value of a Characteristic with this function does two things.  First, it causes HomeSpan to send an Event Notification message back to HomeKit
-  // letting HomeKit know the new value of the Characteristic.  Since messages create network traffic, HomeSpan keeps track of all setVal() changes across
-  // all Services and creates one a single Event Notification message reporting all the changes togther at the end of each polling cycle.
+  // 幸运的是，HomeSpan 使向 HomeKit 回传 Characteristics 值变得简单。在前面的示例中，我们看到了如何使用 getVal() 和 getNewVal() 
+  // 读取 HomeKit 请求的当前和更新的特征值。要执行相反的操作，我们只需使用名为 setVal() 的方法。使用此函数设置 
+  //特征的值有两个作用。首先，它会导致 HomeSpan 向 HomeKit 回传事件通知消息，让 HomeKit 知道特征的新值。
+  // 由于消息会产生网络流量，因此 HomeSpan 会跟踪所有服务中的所有 setVal() 更改，并在每个轮询周期结束时创建一个事件通知消息来报告所有更改。
 
-  // The second thing that HomeSpan does when you change the value of a Characteristic with setVal() is to reset an internal timer for that Characteristic that
-  // keeps track of how long it's been since the last modification, whether from a previous setVal() instruction, or by HomeKit itself via a call to update().
-  // You can query the time since the last modificaton using the method timeVal() which returns the elapsed time in milliseconds.  By calling this function from 
-  // within loop() you can determine when it's time for a new sensor read, or when to perform some other action.
+  // 当您使用 setVal() 更改特征的值时，HomeSpan 执行的第二件事是重置该特征的内部计时器，该计时器会跟踪自
+  // 上次修改以来的时间，无论是来自之前的 setVal() 指令，还是由 HomeKit 本身通过调用 update()。您可以使用方法 timeVal() 查询自上次
+  // 修改以来的时间，该方法返回以毫秒为单位的已用时间。通过从 loop() 中调用此函数，您可以确定何时需要读取新的传感器，或何时执行其他操作。
 
-  // NOTE: It it NOT recommended to continuously change Characteristic values using setVal() as this will generate a lot of network traffic since HomeSpan
-  // sends Event Notifications bck to all registered HomeKit Controllers.  It's fine to perform internal calculations, generate signals on different pins,
-  // and perform any other internal actions you may need as frequently as you require.  But limit the use of setVal() to a reasonable frequency, such as maybe
-  // one per minute for a temperature sensor.  Do not use setVal() unless the value of the Characteristic changes, but do use it to immediately inform HomeKit of
-  // something time-sensitive, such as a door opening, or a smoke alarm triggering.
+  // 注意：不建议使用 setVal() 连续更改特性值，因为这会产生大量网络流量，因为 HomeSpan 会将事件通知发送回所有已注册的 HomeKit 控制器。
+  // 可以执行内部计算、在不同引脚上生成信号以及根据需要频繁执行您可能需要的任何其他内部操作。但请将 setVal() 的使用限制在合理的频率，
+  // 例如对于温度传感器，可能每分钟一次。除非特性值发生变化，否则不要使用 setVal()，但请使用它立即通知 HomeKit 一些时间敏感的事情，
+  // 例如开门或烟雾警报触发。
 
-  // As usual, all of the logic for this example are encapsulated in new standalone derived Services.  You'll find fully-commented definitions for the DEV_TempSensor() and 
-  // the DEV_AirQualitySensor() Services instantiated below, in the DEV_Sensors.h file.  As noted, this example is for instructional purposes only -- we do not actually
-  // connect a Temperature Sensor or Air Quality Sensor to our ESP32 device.  As such, we did not define the Services to take any arguments to specify pin numbers or any
-  // other information needed to implement an actual sensor.  Instead, in order to see how real a device would work, we simulate periodic changes by modifying Characteristic
-  // values using setVal() with either a sequence of repeating values, or random values.  See DEV_Sensors.h for complete details.
+  // 与往常一样，此示例的所有逻辑都封装在新的独立派生服务中。您将在 DEV_Sensors.h 文件中找到下面实例化的 DEV_TempSensor() 和 DEV_AirQualitySensor() 
+  // 服务的完整注释定义。如前所述，此示例仅用于指导目的 - 我们实际上并未将温度传感器或空气质量传感器连接到我们的 ESP32 设备。因此，我们没有定义服务以
+  // 接受任何参数来指定引脚号或实现实际传感器所需的任何其他信息。相反，为了了解设备的实际工作方式，我们通过使用 setVal() 修改特性值来模拟周期性变化，
+  // 该 setVal() 可以是一系列重复值或随机值。有关完整详细信息，请参阅 DEV_Sensors.h。
 
-  // Once you understand these examples, you should be able to use implement your own loop() method and utilize setVal() along with timeVal() for any combination of
-  // HomeKit Services with Characteristics that require your device to send periodic update messages to HomeKit Controllers, ranging from Smoke Alarms to Door Sensors.
-  
+  // 一旦您理解了这些示例，您应该能够使用自己的 loop() 方法实现并将 setVal() 和 timeVal() 用于任何具有特性的 HomeKit 服务组合，这些特性要求您的设备
+  // 向 HomeKit 控制器发送定期更新消息，范围从烟雾报警器到门传感器。
+
   Serial.begin(115200);
 
   homeSpan.begin(Category::Bridges,"HomeSpan Bridge");
@@ -97,15 +80,15 @@ void setup() {
     new Service::AccessoryInformation();
       new Characteristic::Identify(); 
       new Characteristic::Name("Temp Sensor");
-    new DEV_TempSensor();                                                                // Create a Temperature Sensor (see DEV_Sensors.h for definition)
+    new DEV_TempSensor();                                                                // 创建温度传感器（请参阅 DEV_Sensors.h 了解定义）
 
   new SpanAccessory();
     new Service::AccessoryInformation();
       new Characteristic::Identify(); 
       new Characteristic::Name("Air Quality");  
-    new DEV_AirQualitySensor();                                                          // Create an Air Quality Sensor (see DEV_Sensors.h for definition)
+    new DEV_AirQualitySensor();                                                          // 创建空气质量传感器（请参阅 DEV_Sensors.h 了解定义）
 
-} // end of setup()
+} // setup() 结束
 
 //////////////////////////////////////
 
@@ -113,6 +96,6 @@ void loop(){
   
   homeSpan.poll();
   
-} // end of loop()
+} // loop() 结束
 
 //////////////////////////////////////
