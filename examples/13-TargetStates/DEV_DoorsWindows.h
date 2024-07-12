2aeb752a104c66cb21f084c2b2575d2e4398b718
abackup <40161397+abackup@users.noreply.github.com>
2024-07-12 14:57:40 +0800

    Update DEV_DoorsWindows.h

--- examples/13-TargetStates/DEV_DoorsWindows.h
+++ examples/13-TargetStates/DEV_DoorsWindows.h
@@ -1,78 +1,61 @@
-
 ////////////////////////////////////
-//   DEVICE-SPECIFIC LED SERVICES //
+//       设备专用 LED 服务        //
 ////////////////////////////////////
 
-struct DEV_GarageDoor : Service::GarageDoorOpener {    // A Garage Door Opener
+struct DEV_GarageDoor : Service::GarageDoorOpener {     // 车库门开启器
 
-  Characteristic::CurrentDoorState *current;           // reference to the Current Door State Characteristic (specific to Garage Door Openers)
-  Characteristic::TargetDoorState *target;             // reference to the Target Door State Characteristic (specific to Garage Door Openers)  
-  SpanCharacteristic *obstruction;                     // reference to the Obstruction Detected Characteristic (specific to Garage Door Openers)
+  Characteristic::CurrentDoorState *current;            // 参考当前门状态特征（特定于车库门开启器）
+  Characteristic::TargetDoorState *target;             // 参考目标门状态特征（特定于车库门开启器）
+  SpanCharacteristic *obstruction;        // 参考障碍物检测特性（特定于车库门开启器）
 
-  DEV_GarageDoor() : Service::GarageDoorOpener(){      // constructor() method
-
-    // Below we use enumerated constants rather than integers to set the values of the Characteristics.
-    // Using enumerated constants means not having to remember the integer code for each state.  You'll find
-    // a complete list of all available enumerated constants on HomeSpan's Services and Characteristics page.
-    // Note the use of enumerated constants is optional - you can always use the integer code representing
-    // each state instead.
+  DEV_GarageDoor() : Service::GarageDoorOpener(){       // 构造函数（）方法
         
-    current=new Characteristic::CurrentDoorState(Characteristic::CurrentDoorState::CLOSED);      // here we use the fully-qualified name of the constant "CLOSED"
-    target=new Characteristic::TargetDoorState(target->CLOSED);                                  // here we use the name of the object instead of the fully-qualified name (much less typing)
-
-    // Below we must use the fully-qualified name of the enumerated constant and cannot use "obstruction->NOT_DETECTED".
-    // Why?  Because above we declared "obstruction" to be a pointer to a generic SpanCharacteristic instead of a pointer to 
-    // the more specific Characteristic::ObstructionDetected.  Either is fine, and it's just a matter of programming preference
-    // (as you can see we use both conventions in this sketch).  But the downside of using SpanCharacteristic to declare a
-    // Characteristic that contains enumerated constants is that the object itself does not know about these constants. This is
-    // because all enumerated constants are uniquely defined within their respective specific Characteristic classes, and not in the
-    // generic SpanCharacteristic class from which all specific Characterstics are derived.
+    current=new Characteristic::CurrentDoorState(1);              // 初始值 1 表示关闭
+    target=new Characteristic::TargetDoorState(1);                // 初始值 1 表示关闭
+    obstruction=new Characteristic::ObstructionDetected(false);   // 初始值为 false 表示未检测到任何障碍物
     
-    obstruction=new Characteristic::ObstructionDetected(Characteristic::ObstructionDetected::NOT_DETECTED);   // this works
-//  obstruction=new Characteristic::ObstructionDetected(obstruction->NOT_DETECTED);                           // this would produce a compiler error (try it and see)
-    
-    Serial.print("Configuring Garage Door Opener");   // initialization message
+    Serial.print("Configuring Garage Door Opener");   // 初始化消息
     Serial.print("\n");
 
-  } // end constructor
+  } // 结束构造函数
 
-  boolean update(){                              // update() method
+  boolean update(){                              // update() 方法
 
-    // see HAP Documentation for details on what each value represents
+    // 有关每个值所代表的详细信息，请参阅 HAP 文档
 
-    if(target->getNewVal()==target->OPEN){          // HomeKit is requesting the door to be in OPEN position
+    if(target->getNewVal()==0){                     // 如果目标状态值设置为 0，则 HomeKit 请求门处于打开位置
       LOG1("Opening Garage Door\n");
-      current->setVal(current->OPENING);            // set the current-state value to OPENING
-      obstruction->setVal(false);                   // clear any prior obstruction detection - note we do not bother using an enumerated constant here
+      current->setVal(2);                           // 将当前状态值设置为 2，表示“打开”
+      obstruction->setVal(false);                   // 清除所有先前的障碍物检测
     } else {
-      LOG1("Closing Garage Door\n");                // else HomeKit must be requesting the door to be in the CLOSED position
-      current->setVal(current->CLOSING);            // set the current-state value to CLOSING
-      obstruction->setVal(false);                   // clear any prior obstruction detection
+      LOG1("Closing Garage Door\n");                // 否则，目标状态值设置为 1，并且 HomeKit 请求门处于关闭位置
+      current->setVal(3);                           // 将当前状态值设置为 3，表示“关闭”
+      obstruction->setVal(false);                   // 清除所有先前的障碍物检测
     }
     
-    return(true);                               // return true
+    return(true);                               // 返回 true
   
-  } // update
+  } //  更新
 
-  void loop(){                                     // loop() method
+  void loop(){                                     // loop() 方法
 
-    if(current->getVal()==target->getVal())        // if current-state matches target-state there is nothing do -- exit loop()
+    if(current->getVal()==target->getVal())        // 如果当前状态与目标状态匹配则不执行任何操作——退出循环（）
       return;
 
-    if(current->getVal()==current->CLOSING && random(100000)==0){    // here we simulate a random obstruction, but only if the door is closing (not opening)
-      current->setVal(current->STOPPED);                             // if our simulated obstruction is triggered, set the curent-state to STOPPED
-      obstruction->setVal(true);                                     // and set obstruction-detected to true
+    if(current->getVal()==3 && random(100000)==0){    // 这里我们模拟一个随机障碍物，但前提是门正在关闭（而不是打开）
+      current->setVal(4);                             // 如果我们的模拟阻塞被触发，则将当前状态设置为 4，这意味着“停止”
+      obstruction->setVal(true);                      // 并将“检测到阻塞”设置为 true
       LOG1("Garage Door Obstruction Detected!\n");
     }
 
-    if(current->getVal()==current->STOPPED)                          // if the current-state is stopped, there is nothing more to do - exit loop()     
+    if(current->getVal()==4)                       // 如果当前状态已停止，则无需执行任何操作 - 退出循环（）
       return;
 
-    // This last bit of code only gets called if the door is in a state that represents actively opening or actively closing.
-    // If there is an obstruction, the door is "stopped" and won't start again until the HomeKit Controller requests a new open or close action
+    // 仅当门处于主动打开或主动关闭的状态时，才会调用这最后一部分代码。
+    // 如果有障碍物，门将“停止”，并且不会再次启动，直到 HomeKit 控制器请求新的打开或关闭操作
 
-    if(target->timeVal()>5000)                     // simulate a garage door that takes 5 seconds to operate by monitoring time since target-state was last modified
-      current->setVal(target->getVal());           // set the current-state to the target-state
+    if(target->timeVal()>5000)                     // 通过监控自目标状态上次修改以来的时间来模拟需要 5 秒才能运行的车库门
+      current->setVal(target->getVal());           // 将当前状态设置为目标状态
        
   } // loop
   
@@ -80,50 +63,57 @@ struct DEV_GarageDoor : Service::GarageDoorOpener {    // A Garage Door Opener
 
 ////////////////////////////////////
 
-struct DEV_WindowShade : Service::WindowCovering {     // A motorized Window Shade with Hold Feature
+struct DEV_WindowShade : Service::WindowCovering {     // 具有保持功能的电动窗帘
 
-  SpanCharacteristic *current;                     // reference to a "generic" Current Position Characteristic (used by a variety of different Service)
-  SpanCharacteristic *target;                      // reference to a "generic" Target Position Characteristic (used by a variety of different Service)
+  SpanCharacteristic *current;                     // 引用“通用”当前位置特征（由各种不同的服务使用）
+  SpanCharacteristic *target;                      // 引用“通用”目标位置特征（由各种不同的服务使用）
 
-  DEV_WindowShade() : Service::WindowCovering(){       // constructor() method
+  DEV_WindowShade() : Service::WindowCovering(){       // 构造函数（）方法
         
-    current=new Characteristic::CurrentPosition(0);     // Window Shades have positions that range from 0 (fully lowered) to 100 (fully raised)
+    current=new Characteristic::CurrentPosition(0);     // 窗帘的位置范围从 0（完全降低）到 100（完全升起）
     
-    target=new Characteristic::TargetPosition(0);       // Window Shades have positions that range from 0 (fully lowered) to 100 (fully raised)
-    target->setRange(0,100,10);                         // set the allowable target-position range to 0-100 IN STEPS of 10
+    target=new Characteristic::TargetPosition(0);       // 窗帘的位置范围从 0（完全降低）到 100（完全升起）
+    target->setRange(0,100,10);                         // 将允许的目标位置范围设置为 0-100，步长为 10
         
-    Serial.print("Configuring Motorized Window Shade");   // initialization message
+    Serial.print("Configuring Motorized Window Shade");   // 初始化消息
     Serial.print("\n");
 
-  } // end constructor
+  } // 结束构造函数
 
-  boolean update(){                              // update() method
+  boolean update(){                              // update() 方法
 
-    if(target->getNewVal()>current->getVal()){      // if the target-position requested is greater than the current-position, simply log a "raise" message  
-      LOG1("Raising Shade\n");                      // ** there is nothing more to do - HomeKit keeps track of the current-position so knows raising is required
+    // 以下逻辑基于 HomeKit 在实践中的操作方式，与 HAP 文档不一致。在该文档中，HomeKit 似乎支持完全打开或完全关闭窗帘，
+    // 并带有一个可选控件，用于在窗帘移动时将其保持在给定的中间位置。
+
+    // 实际上，即使您实例化该特性，HomeKit 似乎也没有实现任何形式的“保持”控制按钮。
+    // 相反，HomeKit 提供了一个完整的滑块控件，类似于灯泡的亮度控件，可让您将窗帘的精确位置设置为 0-100%。
+    // 这样就不需要任何类型的“保持”按钮了。由此产生的逻辑也非常简单：
+
+    if(target->getNewVal()>current->getVal()){      // 如果请求的目标位置大于当前位置，则只需记录“提升”消息
+      LOG1("Raising Shade\n");                      // ** 没有其他事情要做 - HomeKit 会跟踪当前位置，因此知道需要提升
     } else 
-    if(target->getNewVal()<current->getVal()){      // if the target-position requested is less than the current-position, simply log a "raise" message  
-      LOG1("Lowering Shade\n");                     // ** there is nothing more to do - HomeKit keeps track of the current-position so knows lowering is required
+    if(target->getNewVal()<current->getVal()){      // 如果请求的目标位置小于当前位置，则只需记录“提升”消息
+      LOG1("Lowering Shade\n");                     // ** 没有其他事情要做 - HomeKit 会跟踪当前位置，因此知道需要降低
     }
         
-    return(true);                               // return true
+    return(true);                               // 返回 true
   
-  } // update
+  } //  更新
 
-  void loop(){                                     // loop() method
+  void loop(){                                     // loop() 方法
 
-    // Here we simulate a window shade that takes 5 seconds to move to its new target posiiton
+    // 这里我们模拟了窗帘需要 5 秒才能移动到新的目标位置
     
-    if(current->getVal()!=target->getVal() && target->timeVal()>5000){          // if 5 seconds have elapsed since the target-position was last modified...
-      current->setVal(target->getVal());                                        // ...set the current position to equal the target position
+    if(current->getVal()!=target->getVal() && target->timeVal()>5000){          // 如果自目标位置上次修改以来已经过去了 5 秒……
+      current->setVal(target->getVal());                                        // ...将当前位置设置为等于目标位置
     }
 
-    // Note there is no reason to send continuous updates of the current position to the HomeKit.  HomeKit does NOT display the
-    // current position.  Rather, it simply compares the value of the current position to the value of target positon as set by the
-    // the user in the Home App.  If it finds current and target positions are the same, it knows the shade is stopped.  Otherwise
-    // it will report the shade is raising or lowering depending on whether the specified target state is greater or less than
-    // the current state.
-   
-  } // loop
+    // 请注意，没有必要将当前位置的连续更新发送到 HomeKit。HomeKit 不会显示当前位置。相反，
+    // 它只是将当前位置的值与用户在 “家庭”应用中设置的目标位置值进行比较。如果它发现当前位置和目标位置相同，
+    // 它就知道窗帘已停止。否则，它将根据指定的目标状态是大于还是小于当前状态来报告窗帘正在升起或降低。
+
+    // 根据 HAP，特征位置状态也是必需的。然而，这似乎是重复的，而且考虑到 HomeKit 使用当前位置的方式，根本不需要。
+    
+  } // 循环
   
 };
