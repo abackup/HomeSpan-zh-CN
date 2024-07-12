fb2dc405e2f6deb42e028c9311844e01d9393c5a
abackup <40161397+abackup@users.noreply.github.com>
2024-07-12 14:53:25 +0800

    Update 10-RGB_LED.ino

--- examples/10-RGB_LED/10-RGB_LED.ino
+++ examples/10-RGB_LED/10-RGB_LED.ino
@@ -1,36 +1,28 @@
 /*********************************************************************************
- *  MIT License
+ *  MIT 许可证
  *  
- *  Copyright (c) 2020-2024 Gregg E. Berman
+ *  Copyright (c) 2020-2022 Gregg E. Berman
  *  
  *  https://github.com/HomeSpan/HomeSpan
  *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ *  特此授予获得此软件和相关文档文件（“软件”）副本的任何人免费许可，以无限制方式处理软件，
+ *  包括但不限于使用、复制、修改、合并、发布、分发、再许可和/或销售软件副本的权利，并允许
+ *  向其提供软件的人员这样做，但须遵守以下条件：
  *  
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ *  上述版权声明和本许可声明均应包含在软件的所有副本或重要部分中。
  *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ *  软件按“原样”提供，不作任何明示或暗示的保证，包括但不限于适销性、特定用途的适用性和不
+ *  侵权性的保证。在任何情况下，作者或版权持有者均不对因软件或使用或其他处理软件而引起的
+ *  或与之相关的任何索赔、损害或其他责任承担责任，无论是合同行为、侵权行为还是其他行为。
  *  
  ********************************************************************************/
  
 ////////////////////////////////////////////////////////////
 //                                                        //
-//    HomeSpan: A HomeKit implementation for the ESP32    //
+//              HomeSpan：ESP32 的 HomeKit 实现           //
 //    ------------------------------------------------    //
 //                                                        //
-// Example 10: Controlling a full-color RGB LED           //
+//              示例 10：控制全彩 RGB LED                  //
 //                                                        //
 //                                                        //
 ////////////////////////////////////////////////////////////
@@ -40,11 +32,9 @@
 
 void setup() {
 
-  // Example 10 illustrates how to control an RGB LED to set any color and brightness.
-  // The configuration below should look familiar by now.  We've created a new derived Service,
-  // called DEV_RgbLED to house all the required logic.  You'll find all the code in DEV_LED.h.
-  // For completeness, this configuration also contains an on/off LED and a dimmable LED as shown
-  // in prior examples.
+  // 示例 10 说明如何控制 RGB LED 以设置任何颜色和亮度。
+  // 下面的配置现在应该看起来很熟悉。我们创建了一个名为 DEV_RgbLED 的新派生服务来容纳所有必需的逻辑。
+  // 您将在 DEV_LED.h 中找到所有代码。为了完整起见，此配置还包含一个开/关 LED 和一个可调光 LED，如前面的示例所示。
   
   Serial.begin(115200);
 
@@ -58,21 +48,21 @@ void setup() {
     new Service::AccessoryInformation();
       new Characteristic::Identify();            
       new Characteristic::Name("Simple LED");
-    new DEV_LED(16);                                                               // Create an On/Off LED attached to pin 16
+    new DEV_LED(16);                                                               // 创建一个连接到引脚 16 的开/关 LED
 
   new SpanAccessory();                                                          
     new Service::AccessoryInformation();    
       new Characteristic::Identify();               
       new Characteristic::Name("Dimmable LED"); 
-    new DEV_DimmableLED(17);                                                       // Create a Dimmable (PWM-driven) LED using attached to pin 17
+    new DEV_DimmableLED(17);                                                       // 使用连接到引脚 17 的 LED 创建可调光（PWM 驱动）LED
 
   new SpanAccessory();                                                          
     new Service::AccessoryInformation();    
       new Characteristic::Identify();               
       new Characteristic::Name("RGB LED"); 
-    new DEV_RgbLED(32,22,23);                                                      // Create an RGB LED attached to pins 32,22,23 (for R, G, and B LED anodes)
+    new DEV_RgbLED(32,22,23);                                                      // 创建一个连接到引脚 32、22、23 的 RGB LED（用于 R、G 和 B LED 阳极）
       
-} // end of setup()
+} // setup() 结束
 
 //////////////////////////////////////
 
@@ -80,4 +70,4 @@ void loop(){
   
   homeSpan.poll();
   
-} // end of loop()
+} // loop() 结束
