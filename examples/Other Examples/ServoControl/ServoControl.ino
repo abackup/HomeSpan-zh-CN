01ce67e6ac7b0997b78717f5f62dd0e214914ccf
abackup <40161397+abackup@users.noreply.github.com>
2024-07-12 15:28:46 +0800

    Update ServoControl.ino

--- examples/Other Examples/ServoControl/ServoControl.ino
+++ examples/Other Examples/ServoControl/ServoControl.ino
@@ -1,34 +1,24 @@
 /*********************************************************************************
- *  MIT License
+ *  MIT 许可证
  *  
- *  Copyright (c) 2020-2022 Gregg E. Berman
+ *  Copyright (c) 2020-2024 Gregg E. Berman
  *  
  *  https://github.com/HomeSpan/HomeSpan
  *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ *  特此授予获得此软件和相关文档文件（“软件”）副本的任何人免费许可，以无限制方式处理软件，
+ *  包括但不限于使用、复制、修改、合并、发布、分发、再许可和/或销售软件副本的权利，并允许
+ *  向其提供软件的人员这样做，但须遵守以下条件：
  *  
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
+ *  上述版权声明和本许可声明均应包含在软件的所有副本或重要部分中。
  *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
+ *  软件按“原样”提供，不作任何明示或暗示的保证，包括但不限于适销性、特定用途的适用性和不
+ *  侵权性的保证。在任何情况下，作者或版权持有者均不对因软件或使用或其他处理软件而引起的
+ *  或与之相关的任何索赔、损害或其他责任承担责任，无论是合同行为、侵权行为还是其他行为。
  *  
  ********************************************************************************/
  
-// This example demonstrates how to control a real-world Servo Motor using HomeSpan's
-// ServoPin Class, as included in "extras/PwmPin.h"  The code builds upon the
-// WindowShade Accessory from Example 13 by adding a Horizontal Tilt Characteristic that
-// is controlled by a Servo connected to the ESP32.
+// 此示例演示了如何使用 HomeSpan 的 ServoPin 类（包含在“extras/PwmPin.h”中）控制真实世界的伺服电机。
+// 该代码基于示例 13 中的 WindowShade 配件，通过添加由连接到 ESP32 的伺服器控制的水平倾斜特性。
 
 #include "HomeSpan.h" 
 #include "DEV_DoorsWindows.h" 
@@ -47,9 +37,9 @@ void setup() {
     new Service::AccessoryInformation();
       new Characteristic::Identify(); 
       new Characteristic::Name("Window Shade");
-    new DEV_WindowShade(18);                                                            // Create a motorized Window Shade with a Servo attached to Pin 18 that controls the Horizontal Tilt of the Shade
+    new DEV_WindowShade(18);                                                            // 创建一个电动窗帘，并将伺服器连接到引脚 18，以控制窗帘的水平倾斜度
 
-} // end of setup()
+} // 设置结束()
 
 //////////////////////////////////////
 
@@ -57,4 +47,4 @@ void loop(){
   
   homeSpan.poll();
   
-} // end of loop()
+} // 循环结束()
